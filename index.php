404aa07bc913e9590f2e3e3a191ae9425822f2ab
nick990 <nick990>
2014-08-01 11:46:00 +0200

    checkboxes checked by default

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
-<?php
-	/*
-	 * Nella sessione salvo gli ID dei calendari da visualizzare
-	 * $_SESSION['cals_id'] è larray delle stringhe che rappresentano gli ID
-	 * Per default li ha tutti, dovrò fare una funzione che li aggiunge tutti
-	 */
-	session_start();
-	$_SESSION['cals_id']=array();
-	/*
-	$_SESSION['cals_id'][0]=1;
-	$_SESSION['cals_id'][1]=2;
-	*/
-?>
+<?php require "php/session_initial.php" ?>
 <html>
 	<head>
 		
 		<?php 
 		require("php/link_CSS.php"); ?>
 		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-			<script type="text/javascript" src="js/event_popup.js"></script>
+		<script type="text/javascript" src="js/event_popup.js"></script>
 		<script type="text/javascript" src="js/jsfun.js"></script>
 	
 		
